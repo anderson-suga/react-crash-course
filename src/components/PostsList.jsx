bda87a639d5387ef641494ae804e067fb7b966bf
Anderson Suga <anderson.suga@gmail.com>
2023-12-29 16:17:36 -0300

    31. Handling Form Submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
@@ -7,30 +5,14 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBoby, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredBoby} body={enteredAuthor} />
         <Post author="Maximilian" body="Check out the full course!" />
       </ul>
     </>

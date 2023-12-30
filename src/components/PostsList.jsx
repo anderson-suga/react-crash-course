300a3414ee2b68f6b0df7515df68e5107d8ea71e
Anderson Suga <anderson.suga@gmail.com>
2023-12-30 11:09:18 -0300

    33. Outputting List Data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,7 +7,7 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const { posts, setPosts } = useState([]);
+  const [posts, setPosts] = useState([]);
 
   function addPostHandler(postData) {
     setPosts((existingPosts) => [postData, ...existingPosts]);
@@ -20,9 +20,19 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Maximilian" body="Check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.author} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
